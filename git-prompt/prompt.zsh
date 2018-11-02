85b8e89c6e2b1ac357dab3f9c37fbb24e84d1c8b
Dom Slee <domslee97@gmail.com>
2018-11-02 18:00:27 +1100

    Add caching for git status

--- git-prompt/prompt.zsh
+++ git-prompt/prompt.zsh
@@ -1,2 +1,6 @@
 source ~/.zsh-git-prompt/zshrc.sh
-PROMPT='%B%m%~%b$(git_super_status) %# '
+# this is a bit...
+git_super_status >/dev/null
+export ZSH_THEME_GIT_PROMPT_CACHE=1
+PROMPT='%B%~%b%{$reset_color%}$(git_super_status) %# '
+#PROMPT='%{$fg[green]%}%~%{$fg_bold[blue]%}$(git_super_status)%{$reset_color%} %# '
