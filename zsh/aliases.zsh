9d81ab07a0ec79dc45c0070201084ab2c12b4ffd
Dom Slee <domslee97@gmail.com>
2019-02-05 17:46:30 +1100

    Fix chrome alias to use open -a

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,6 @@ alias 2018s2="cd ~/Documents/2018s2"
 # don't remember?
 export DISPLAY=:0
 
-
 PATH=$PATH:/opt/local/bin
 alias rm='safe-rm'
 PATH=$PATH:~/Documents/2018s2/COMP6741/assignments/a4/graph-viz
@@ -15,6 +14,6 @@ PATH=$PATH:~/Documents/git/ch
 function skim() {
     /Applications/Skim.app/Contents/MacOS/Skim $@ &
 }
-alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+alias chrome='open -a "Google Chrome"'
 
 
